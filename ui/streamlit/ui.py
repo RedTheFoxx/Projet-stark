5e3c993027d608f6396202d0659d05979b1616e6
RedTheFoxx <morisseau.jeremy@gmail.com>
2024-07-11 15:25:44 +0200

    / ajusté le format du code source

--- ui/streamlit/ui.py
+++ ui/streamlit/ui.py
@@ -1,17 +1,22 @@
 import streamlit as st
 
-st.set_page_config(layout="wide", page_title="Stark - Compte-rendus") # Mode large par défaut de streamlit et titre de page
+st.set_page_config(
+    layout="wide", page_title="Stark - Compte-rendus"
+)  # Mode large par défaut de streamlit et titre de page
 
 # Markdown simple
-st.markdown("""
+st.markdown(
+    """
 # Stark - Rapports et compte-rendus
-
 *Bienvenue sur votre interface de génération automatisée de rapports.*
-""")
+"""
+)
 
-st.markdown("---") # Une barre de séparation en markdown
+st.markdown("---")  # Une barre de séparation en markdown
 
-col1, col2 = st.columns(2) # Deux colonnes pour aligner le champs d'upload et les radio boutons
+col1, col2 = st.columns(
+    2
+)  # Deux colonnes pour aligner le champs d'upload et les radio boutons
 
 with col1:
     st.subheader("Mode")
@@ -23,8 +28,8 @@ with col1:
 with col2:
     st.subheader("Fichiers")
     uploaded_files = st.file_uploader(
-        "Déposez vos fichiers au format .csv ou .txt",
-        type=["csv", "txt"],
+        "Déposez vos fichiers au format .xlsx ou .txt",
+        type=["xlsx", "txt"],
         accept_multiple_files=True,
     )
 
@@ -32,12 +37,12 @@ st.markdown("---")
 
 
 if st.button("Générer", type="primary", use_container_width=True):
-    
+
     # 1. Ici, on insérera la logique cablée à un Langchain (ou autre) pour gérer deux pipes en fonction du mode sélectionné sur le radio button.
     # - Pour l'instant c'est du code de démo pour faire marcher l'interface joliment
-    
+
     # Si on a le temps, on fait le pipe 3 avec les deux fichiers .txt
-    
+
     with st.spinner("Génération du rapport en cours ..."):
 
         import time
@@ -51,11 +56,13 @@ if st.button("Générer", type="primary", use_container_width=True):
             file_name="rapport.txt",
             mime="text/plain",
         )
-        
+
 # 2. A ce niveau, on pourrait ajouter une étape intermédiare pour prévisualiser le rapport avant de le télécharger
 # - et proposer à l'utilisateur d'en regénérer un si ça ne convient pas.
 
 st.markdown("---")
 
 # J'avais envie
-st.caption("Vince & Red - Stark © 2024 / Toute utilisation de système basé sur de l'IA générative implique des principes de précaution et de responsabilité.")
+st.caption(
+    "Vince & Red - Stark © 2024 / Toute utilisation de système basé sur de l'IA générative implique des principes de précaution et de responsabilité."
+)
