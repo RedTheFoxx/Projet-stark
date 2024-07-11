7b2bae0d2d0630e1c65e301c110458124ce137e3
RedTheFoxx <morisseau.jeremy@gmail.com>
2024-07-11 17:21:59 +0200

    + corps de docify

---
+++ engine/docify.py
@@ -0,0 +1,11 @@
+"""Module de génération de rapports au format docx."""
+
+from docx import Document
+
+def make_intervention_report(header, intervention_report):
+    doc = Document()
+    pass
+
+def make_activity_report(x,y):
+    doc = Document()
+    pass
