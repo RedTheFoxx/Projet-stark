f45b9a98cca92adedc2b19dd73e2814f11f73130
RedTheFoxx <morisseau.jeremy@gmail.com>
2024-07-12 18:28:36 +0200

    / prise en charge partielle cra

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ import streamlit as st
 import engine.docify as docify
 import engine.extractor as extractor
 import engine.llm_mode_1 as llm_mode_1
+import engine.llm_mode_3 as llm_mode_3
 
 st.set_page_config(
     layout="wide", page_title="Stark - Compte-rendus"
 )  # Mode large par défaut de streamlit et titre de page
 
 # Markdown simple
+st.image("ressources/data/logo_stark.png", width=100)   
 st.markdown(
     """
 # Stark - Rapports et compte-rendus
@@ -45,19 +47,50 @@ if st.button("Générer", type="primary", use_container_width=True):
             for file in uploaded_files or []:
                 nb_records = extractor.count_records(file)
                 st.write(f"Nombre d'enregistrements détectés : {nb_records}")
-                
+
                 all_data = extractor.extract_all_lines(file)
                 for i, data_to_process in enumerate(all_data):
                     with st.spinner(f"Génération du rapport {i+1}/{nb_records} ..."):
                         generated_content = llm_mode_1.cook_report(data_to_process)
-                        docify.make_intervention_report(generated_content, f"rapport_intervention_{i+1}")
-                
+                        docify.make_intervention_report(
+                            generated_content, f"rapport_intervention_{i+1}"
+                        )
+
                 st.success(f"{nb_records} rapport(s) généré(s) avec succès !")
 
     if mode == "CR d'activité":
-        st.write("Mode d'activité non implémenté")
-    if mode == "CR avec gammes (non-implémenté)":
-        st.write("Mode gammes non implémenté")
+        filters = [
+            "DIEPPE CHATEAU MUSEE",
+            "DIEPPE COMMUN D'AGGLO SERVICE COLLECTE",
+            "DIEPPE GYMNASE ROGE DESJARDIN MILLE CLUB",
+            "NEUVILLE LES DIEPPE MATERNELLE MARIE CUR",
+            "SERQUEUX MAIRIE ECOLE",
+            "DIEPPE HOTEL WINDSOR"
+        ]
+        for file in uploaded_files or []:
+            with st.spinner("Extraction des données ..."):
+                
+                    customer_records = extractor.extract_per_customer(file, filter)
+                    
+                    # if customer_records != [""]:
+                        
+                    #     st.write(f"Nombre d'interventions pour le client {filter} : {len(customer_records)}")
+                    #     with st.spinner("Génération des rapports ..."):
+                            
+                    #         #TODO : On fait un résumé par ligne d'intervention
+                    #         all_interventions = []
+                    #         for i, data_to_process in enumerate(customer_records):
+                    #             generated_interventions = llm_mode_3.cook_report_interventions(data_to_process)
+                    #             all_interventions.append(generated_interventions)
+                            
+                    #         #TODO : On envoie tout le dico. des interventions au second prompt pour faire un bilan
+                    #         # generated_content = llm_mode_3.cook_report_resume(all_interventions)
+                            
+                    #         #TODO : On créé le docx
+                    #         # docify.make_activity_report(filter, all_interventions, generated_content, f"rapport_activite_{i+1}")
+                    # else:
+                    #     st.error(f"Aucune donnée trouvée pour le client {filter}")
+                    #     st.stop()
 
 st.markdown("---")
 
