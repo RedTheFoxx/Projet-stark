55cc73d85eae4ac4a8624db5c212204a367de40b
RedTheFoxx <morisseau.jeremy@gmail.com>
2024-07-13 10:23:05 +0200

    + màj formattage (vincent)

--- engine/docify.py
+++ engine/docify.py
@@ -12,14 +12,20 @@ def make_intervention_report(data, file_name):
         data
     )  # On s'assure de convertir la string en entrée en dictionnaire Python
     doc = Document()
+    # Accès à la première section du document
+    section = doc.sections[0]
+    # Définition des marges en pouces
+    section.top_margin = Inches(0.5)
+    section.bottom_margin = Inches(0.5)
+    section.left_margin = Inches(0.75)
+    section.right_margin = Inches(0.75)
 
     header_table = doc.add_table(
-        rows=1, cols=2
+        rows=1, cols=3
     )  # Case 1 : la date d'intervention et case 2, le logo de stark
-    header_table.cell(0, 0).text = (
-        "Date d'intervention : " + data["Date d'intervention"]
-    )
-    logo_cell = header_table.cell(0, 1)
+    header_table.cell(0, 0).text = "\n\n\nDate d'intervention :"
+    header_table.cell(0, 1).text = "\n\n\n" + data["Date d'intervention"]
+    logo_cell = header_table.cell(0, 2)
     logo_path = "ressources/data/logo_stark.png"
     logo_paragraph = logo_cell.paragraphs[0]
     logo_run = logo_paragraph.add_run()
@@ -28,23 +34,35 @@ def make_intervention_report(data, file_name):
     doc.add_heading("Rapport d'intervention", 0)
     doc.add_heading("Informations client", 2)
 
-    table = doc.add_table(
-        rows=len(data) - 1, cols=2
-    )  # - 1 pour éviter d'avoir une ligne vide à la fin vu qu'on place "resume" plus bas
+    table = doc.add_table(rows=len(data) - 1, cols=2)
+    for cell in table.columns[0].cells:
+        cell.width = Inches(1.25)
+    for cell in table.columns[1].cells:
+        cell.width = Inches(6)
 
     for i, (key, value) in enumerate(data.items()):
-        if key not in ["Resume", "Date d'intervention"]:
+        if key not in ["Resume", "Date d'intervention", "Intervenant"]:
             table.cell(i, 0).text = key
             table.cell(i, 1).text = value
 
-    doc.add_heading("Compte-rendu d'intervention", 2)
+    doc.add_heading("Compte-rendu d'intervention\n", 2)
     doc.add_paragraph(data["Resume"])
+    doc.add_paragraph(
+        """Nous tenons à vous remercier pour votre confiance et restons à votre disposition pour toute information complémentaire.
+N'hésitez pas à contacter le Centre de Relation Clients (CRC) au 0 800 80 93 00, disponible 24h/24 et 7j/7.
+Nous tenons également à souligner l'importance de faire suivre votre matériel par un professionnel pour garantir sa qualité et sa durabilité.
 
+Cordialement,"""
+    )
+
+    doc.add_paragraph(data["Intervenant"])
     doc.save(f"output/{file_name}.docx")
 
 
-def make_activity_report(data_customer_site, data_interventions, data_report, file_name):
-    
+def make_activity_report(
+    data_customer_site, data_interventions, data_report, file_name
+):
+
     # data_interventions = json.loads(data_interventions)
     # data_report = json.loads(data_report)
     # data_customer_site = json.loads(data_customer_site)
@@ -112,4 +130,6 @@ if __name__ == "__main__":
     data_report = "BLABLABLA CECI EST UN RESUME ANNUEL BLABLABLA"
 
     # make_intervention_report(test_data, "test_rapport")
-    make_activity_report(data_customer_site, data_interventions, data_report, "test_rapport")
+    make_activity_report(
+        data_customer_site, data_interventions, data_report, "test_rapport"
+    )
