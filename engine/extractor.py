9ec1cec49ff19c670915084a7337b95e8f942180
RedTheFoxx <morisseau.jeremy@gmail.com>
2024-07-14 17:30:57 +0200

    + docstrings

--- engine/extractor.py
+++ engine/extractor.py
@@ -1,9 +1,8 @@
 """Gestion des extractions de données d'un fichier Excel et leur conversions."""
 
-import pathlib
 import pandas as pd
 import json
-
+import os
 
 def extract_all_lines(file) -> dict:
     """Extrait toutes les lignes d'un fichier Excel et les convertit en un dictionnaire.
@@ -44,8 +43,17 @@ def count_records(file) -> int:
     df = pd.read_excel(file)
     return len(df)
 
-#TODO : Toutes les valeurs null doivent être remplacées par des chaines vides
-def extract_per_customer(file, filter) -> dict:
+def extract_per_customer(file, filter : str) -> dict:
+    """Extrait les données d'un fichier Excel filtrées par client.
+
+    Args:
+        file: Le fichier à traiter.
+        filter: Une string qui représente la valeur de colonne "Libellé site" que l'on veut considérer.
+        
+    Returns:
+        Un dictionnaire orientation RECORDS contenant les données filtrées par client.
+    """
+    
     df = pd.read_excel(file)
     df = df[df["Libellé site"] == filter]
     df.to_json(
@@ -54,9 +62,9 @@ def extract_per_customer(file, filter) -> dict:
         force_ascii=False,
         date_format="iso",
     )
-    with open(f"temp/extracted_data_per_customer.json", "r", encoding="utf-8") as file:
-        data = json.load(file)
+    with open(f"temp/extracted_data_per_customer.json", "r", encoding="utf-8") as json_file:
+        data = json.load(json_file)
     return data
 
 if __name__ == "__main__":
-    print(extract_per_customer(pathlib.Path("ressources/data/extraits-Data-Interventions-annuels_1.xlsx"), "DIEPPE COMMUN D'AGGLO SERVICE COLLECTE"))
+    print(extract_per_customer(os.path.join("ressources/data/extraits-Data-Interventions-annuels_1.xlsx"), "DIEPPE CHATEAU MUSEE"))
